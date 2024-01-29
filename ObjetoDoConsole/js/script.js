554faa6154063720ae7c93fffca2f3c0386dbbb8
jumalisilva <juliamalisilva@gmail.com>
2024-01-29 14:10:33 -0300

    UC13

---
+++ ObjetoDoConsole/js/script.js
@@ -0,0 +1,75 @@
+// Função de prompt
+
+// const nome = prompt("Qual o seu nome?");
+// console.log(nome);
+
+// const idade = prompt("Qual a sua idade?")
+// console.log(idade);
+
+// Função de alert
+
+// alert("Erro ao preencher o formulário.");
+
+// Funções numéricas - Retornando o valor absoluto de 5
+
+// const abs = Math.abs(-5);
+// console.log(abs);
+
+// Arredondar 3.5 para o inteiro mais próximo
+
+// const round = Math.round(3.5);
+// console.log(round);
+
+// Retornar a raíz quadrada de 16
+
+// const sqrt = Math.sqrt(16);
+// console.log(sqrt);
+
+// Elevando 2 ao cubo
+
+// const pow = Math.pow(2,3);
+// console.log(pow);
+
+// Exibindo uma mensagem de aviso no console
+
+// console.warn("Atenção: esta é uma mensagem de aviso.");
+
+// Exibindo uma mensagem de erro no console
+
+// console.error("Ocorreu um erro.");
+
+// Exibindo uma mensagem informativa no console
+
+// console.info("Esta é uma mensagem informativa.");
+
+// Criando um grupo de mensagem no console
+
+// console.group("Mensagem de teste.");
+// console.log("Esta é uma mensagem de teste.");
+// console.log("Esta é outra mensagem de teste.");
+// console.groupEnd();
+
+// Continue no laço For
+
+// for (let i = 1; i <= 7; i++) {
+//     // Pular a impressão do número 5
+//     if (i === 5) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// Function
+
+// function multiplicar(num1, num2) { // declarando uma função de multiplicação
+    // return num1 * num2 // retornando a multiplicação
+// }
+// console.log(multiplicar(3, 7)); // exibir no console
+
+// Arrow function
+
+/* const multiplicar = (num1, num2) => {
+    return num1 * num2
+}
+console.log(multiplicar(3, 7)); */
+
