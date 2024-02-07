9631daf9703f5d8998edf70c3c5776593b56ab14
jumalisilva <juliamalisilva@gmail.com>
2024-02-07 16:27:53 -0300

    UC13

--- Object-literals/js/script.js
+++ Object-literals/js/script.js
@@ -102,3 +102,81 @@ const codigo = "ABC";
 // Preencher com traços para atingir 5 caracteres
 const codigoPreenchido = codigo.padStart(5, "-");
 console.log(codigoPreenchido);
+
+// Split
+const texto = "Essa é uma string com espaços em branco";
+// Divide a string em um array de letras, usando espaços como separadores
+const letras = texto.split("");
+console.log(letras);
+
+// Divide a string em um array de números, usando vírgulas como separadores
+const numeros2 = "1, 2, 3, 4, 5".split("");
+console.log(numeros2);
+
+// Divide o texto em string separadas por palavras
+const partes = texto.split(" ", 8);
+console.log(partes);
+
+// Join - transforma um array em uma string
+const numerosJoin = [1, 2, 3, 4, 5];
+// Junta os números com vírgulas como separador, tornando uma string
+const listaNumeros = numerosJoin.join();
+console.log(numerosJoin);
+console.log(typeof numerosJoin);
+console.log(listaNumeros);
+console.log(typeof listaNumeros);
+
+//Junta com os números com traços como separador, tornando uma string
+const listaNumerosTracos = numerosJoin.join("-");
+console.log(listaNumerosTracos);
+
+// Transforma os elementos de um array em string e acrescenta um elemento de string
+const frutas = ["maçã", "banana", "laranja"];
+const textoFrutas = frutas.join(" e ");
+console.log(textoFrutas);
+
+// Repeat
+const testandoRepeat = "Olá";
+
+// Repetindo a string 3 vezes
+const textoRepetido = testandoRepeat.repeat(3);
+console.log(textoRepetido);
+
+// Repetindo a string zero vezes
+const textoVazio = testandoRepeat.repeat(0);
+console.log(textoVazio);
+
+// Rest operator
+function minhaFuncao (a, b, ...args) {
+    console.log(a);
+    console.log(b);
+    console.log(args);
+}
+
+minhaFuncao(1, 2, 3, 4, 5);
+
+// Iterando sobre um array
+const frutasA = ["maca", "uva", "melância"];
+
+for(const fruta of frutasA) {
+    console.log(fruta);
+}
+
+// Desestruturando objetos
+const usuario = {
+    primeiroNome: "José",
+    sobrenome: "Xavier",
+    profissao: "Pedreiro"
+}
+
+const [primeiroNome, sobrenome, profissao] = Object.values(usuario);
+console.log(primeiroNome, sobrenome, profissao);
+
+// Desestruturando array
+const array = ["João", "Maria", "Pedro"];
+
+const [primeiro, segundo, terceiro] = Object.values(array);
+console.log(primeiro, segundo);
+
+const [quarto, quinto, sexto] = array;
+console.log(quarto, quinto);
